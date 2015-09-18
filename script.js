2a521c365298dee7ff524ca230989919bf568117
iftiAhmed <iftitech@yahoo.com>
2015-09-17 17:04:47 -0700

    This is my first commit to the repo

---
+++ script.js
@@ -0,0 +1,294 @@
+
+
+/* Part-2
+    Assignment1
+    JavaScript File
+    script.js
+ */
+//  1
+/* ======================================
+var someMonth;
+function theMonth(){}		                                     // function to return current month
+CURRENT_MONTH : September; 		                         // a constant
+var summerMonth = [ " June", "July", "August"]; 		         // an array of summer months
+function myLibraryaFunction(){}	                                 // a function 
+*/ ======================================
+//  2 
+
+// numeric literal
+
+// 12345
+
+//  string literal
+
+/* 
+"California";
+console.log("California" + " is a 2nd biggest state in US in an area wise.");
+*/
+
+//  boolean literal
+/*
+false;
+console.log(false + " is boolean Literal in JavaScript");
+*/
+//  string literal
+/*
+null;    // represent an empty value
+*/
+    
+
+// 3 Complex/variable  expressionexample
+
+/*=======================================================
+var complexExpression1 = ((10 + 2)*3.5)/(2+2);
+
+var complexExpression2 = (Area + 2/Diameter) + 5*2;
+
+*/======================================================
+
+
+// 4 Varable declarations
+
+/* ===========================================
+var sFirstName;
+var sLastName;
+var nAddress;
+var sCity;
+var cState;
+var nZipCode
+var nYourAge;
+var sReferralResource;
+var sMayWeContactYou;
+
+*/===========================================
+
+// 5 
+
+/* =========================================
+var sFirstName = "Iftikhar";
+var sLastName = "Ahmed";
+var sCity = "San Diego";  
+
+*/==========================================
+
+// 6
+
+
+/*
+var yearsOfExperience = 10;
+console.log("I have " + yearsOfExperience + " in Technology.");
+*/
+
+/*
+var secondVariable = true;
+console.log("I did have pass the exam in first attempt is " + secondVariable +".");
+*/
+
+
+var thirdVaraible = 500;
+console.log("I worked with fortune " + thirdVariable + " company is" true);
+
+
+// 7 Hoisting
+
+/*
+color = "Green";
+var color;
+console.log(color);
+*/
+
+
+//  8 
+// Not a valid String Literal
+// var someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I'm not sure about the former."';
+
+/*
+var someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'\m not sure about the former."';
+console.log(someString);
+*/
+
+// 9
+
+/*=================================================
+// Null Variable
+var a = 1;
+var b = null;
+var a = b;
+console.log(a);
+*/================================================
+    
+
+// Undefined Variable
+/*================================================
+var a;
+console.log(a);
+*/================================================
+    
+    
+// 10
+
+/* ==============================================
+typeof x = "iftikhar";
+console.log(x);
+
+typeof y = 3.14;
+console.log(y);
+
+typeof z = false;
+console.log(z);
+
+
+typeof new Date();
+console.log(Date());
+
+typeof myCar;
+console.log(myCar)
+*/==============================================
+
+// 11
+
+/* ============================================
+
+alert("Hello" + "Zak Ruvaicaba" + ",  welcome to the JavaScript class!");
+*/ =============================================
+
+// 12
+/*
+var name = "Iftikhar Ahmed";
+alert("Hello" + name + ",  welcome to the JavaScript class!");
+*/ =============================================
+
+// 13
+
+/*
+var name = "Iftikhar Ahmed";
+var course = "JavaScript";
+
+alert("Hello" + name + ",  welcome to the " + course + " class!");
+*/
+    
+// 14
+alert("Hello Zak Ruvaicaba.\n Welcome to the JavaScript class!");
+
+
+// 15 
+var name;
+prompt("Please enter your name!");
+
+//  16
+var course;
+prompt("What class are you taking in this semester?")
+
+//17
+
+var x = 10;
+var y = 20;
+var sum = x + y;
+console.log(sum);
+
+
+// 18
+
+var x = 20;
+x += 20;
+console.log(x);
+
+// 19
+
+var x = 20;
+x *= 5;
+console.log(x);
+
+// 20
+var x = 20 % 3;
+x = x/1;
+console.log(x);
+
+
+// 21 True
+
+/*
+// Comparsion Operators
+
+var x = 2;
+var y = 5;
+console.log(x < y);
+
+var x = 2;
+var y = 5;
+console.log(x <= y);
+
+var x = 5;
+var y = 5;
+console.log(x == y);
+*/
+
+// Logical Operators
+
+/*
+var x = 5;
+var y = 5;
+console.log(x == 5 && y == 5);
+
+var x = 2;
+var y = 5;
+console.log(x == 2 || y == 5);
+
+var x = 2;
+var y = 5;
+console.log(x <= y != true);
+*/
+
+
+// 22 False
+
+var x = 2;
+var y = 5;
+console.log(x > y);
+
+var x = 2;
+var y = 5;
+console.log(x >= y);
+
+var x = 5;
+var y = 15;
+console.log(x == y);
+*/
+
+// Logical Operators
+
+/*
+var x = 5;
+var y = 15;
+console.log(x == 5 && y == 5);
+
+var x = 2;
+var y = 5;
+console.log(x == 2 || y == 15);
+
+var x = 2;
+var y = 5;
+console.log(x <= y != false);
+*/
+
+// 23
+var myWidget  = new widget("iPad", "iPhone");
+console.log(typeof(myWidget));
+
+//24
+
+var myWidget  = new widget("iPad", "iPhone");
+console.log(myWidget instanceof widget);
+
+// 25
+var myWidget  = new widget("iPad", "iPhone");
+console.log(myWidget instanceof String);
+
+
+
+
+
+
+
+
+
